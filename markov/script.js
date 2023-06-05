b4923069a451a720167cdb30c2224a7bb39d9a04
mmToscano <107902881+mmToscano@users.noreply.github.com>
2023-06-05 15:14:32 -0300

    Add files via upload

--- markov/script.js
+++ markov/script.js
@@ -5,17 +5,10 @@ let matrizDeTransicao = [[],[],[]];
 let matrizDeEstado = [];
 let matrizDeResultado = [];
 
-
-
-function multiplicar(){
-    pegarInfo();
-}
-
-function pegarInfo(){
+function multiplicarMatrizes(){
     //loop para pegar a info da matriz de transição
     for(let linha = 0; linha < cs(".matrizDeTransicao tr").length; linha++){
         for(let colunaCelula = 0; colunaCelula < cs(".matrizDeTransicao tr")[linha].querySelectorAll("td").length; colunaCelula++){
-            //console.log("linha: " + linha + ", " + "colunaCelula: " + colunaCelula);
             let valorCelula = +cs(".matrizDeTransicao tr")[linha].querySelectorAll("td input")[colunaCelula].value;
 
 
@@ -29,15 +22,36 @@ function pegarInfo(){
         matrizDeEstado.push(valorCelula)
     }
 
+    //multiplicar matrizes
     matrizDeResultado[0] = 0;
     matrizDeResultado[1] = 0;
     matrizDeResultado[2] = 0;
-    //multiplicar
+
     for(let linha = 0; linha < 3; linha++){
         for(let colunaCelula = 0; colunaCelula < 3; colunaCelula++){
-            matrizDeResultado[linha] += matrizDeEstado[linha] * matrizDeTransicao[[linha]][colunaCelula];
+            matrizDeResultado[linha] += matrizDeEstado[colunaCelula] * matrizDeTransicao[[colunaCelula]][linha];
         }
     }
 
-    console.log(matrizDeResultado)
+    //testar para ver se os valores não passam de 1.0
+
+    matrizDeTransicao.forEach((item, index) =>{
+        if(somaVetor(item) > 1){
+            throw new Error("Value bigger than 1")
+        }
+    })
+
+    if(somaVetor(matrizDeEstado) > 1){
+        throw new Error("Value bigger than 1")
+    }
+
+    //atribuir resultado no vetor de resultado
+    let vetorDeResultado = cs(".matrizDeResultado td");
+    vetorDeResultado[0].innerHTML = matrizDeResultado[0];
+    vetorDeResultado[1].innerHTML = matrizDeResultado[1];
+    vetorDeResultado[2].innerHTML = matrizDeResultado[2];
+}
+
+function somaVetor(array) {
+    return array[0] + array[1] + array [2];
 }
